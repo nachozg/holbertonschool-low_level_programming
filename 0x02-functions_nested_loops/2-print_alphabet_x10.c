f438e76f1a79c2825a60683794a9b681aa67aed5
nachozg <3351@holbertonschool.com>
2021-06-23 09:30:53 -0700

    2func

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - prints the alphabet in lowercase
+ *
+ *Description: prototype of _putchar
+ *Return: - if return 0
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int i, j;
+	for (j = 0; j <= 9; j++)
+
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
