d82205fcb1adefa0eacda2e70333a51f763cd1fa
nachozg <3351@holbertonschool.com>
2021-06-24 10:53:53 -0700

    2dubing

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,15 +14,15 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && b > c)
 	{
-		largest = 972;
+		largest = a;
 	}
-	else if (b > a && a > c)
+	else if (b >= a && a >= c)
 	{
-		largest = -98;
+		largest = b;
 	}
 	else
 	{
-		largest = 0;
+		largest = c;
 	}
 
 	return (largest);
