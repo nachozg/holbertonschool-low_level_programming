c5b3f53ad27763fcdb3d61cee31847b4680bced0
nachozg <3351@holbertonschool.com>
2021-06-27 14:25:08 -0700

    6 var

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,13 @@
 
 int main(void)
 {
-	char i;
-	for (i = 'z' ; i >= 'a' ; i--)
+	int i;
+	char j;
+
+	for (i = 48; i <= 57; i++)
 		putchar(i);
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
